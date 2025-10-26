8f421d9e19743f4d9fca21daf43b2b28fbe699f9
skcbroo <skcbroo@gmail.com>
2025-10-26 19:06:39 -0300

    Update jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,14 +3,12 @@ import { ENV } from "../config/env.js";
 import { JWTPayload } from "../types/index.js";
 
 const secret: Secret = ENV.JWT_SECRET;
-const signOptions: SignOptions = { expiresIn: ENV.JWT_EXPIRES_IN };
+const signOptions: SignOptions = { expiresIn: ENV.JWT_EXPIRES_IN as any }; // 👈 fix TS
 
-// 🧱 Gera token
 export const generateToken = (payload: JWTPayload): string => {
   return jwt.sign(payload, secret, signOptions);
 };
 
-// 🧩 Verifica token
 export const verifyToken = (token: string): JWTPayload | null => {
   try {
     const decoded = jwt.verify(token, secret) as JWTPayload;
